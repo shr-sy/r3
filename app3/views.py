1e50816f31112494bdd86e9dd0ac184b37ff9cb9
shr-sy <82036296+shr-sy@users.noreply.github.com>
2023-11-15 15:56:26 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def fun3(request):
+    return HttpResponse('I like to watch classical dance too')
